74b3d98b26a48edcddd353c870233fd82af27123
jojo892 <johanna.rompf02@gmail.com>
2023-01-24 14:22:41 +0100

    added MenueVersion1

--- lib/screens/menue.dart
+++ lib/screens/menue.dart
@@ -7,20 +7,46 @@ class Menue extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Little Savior", style: TextStyle(color: Colors.white),),
-        backgroundColor: Color.fromRGBO(8, 83, 59, 1.0),
+        title: Text(
+          "Little Savior",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color.fromRGBO(11, 110, 79, 1.0),
       ),
       drawer: Drawer(
+        backgroundColor: Color.fromRGBO(230, 243, 236, 1.0),
         child: ListView(
           children: [
-            const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(8, 83, 59, 1.0),
-                ),
-                child: Text("Menü"))
+            _buildListTile(title: 'Dashboard'),
+            _buildListTile(title: 'Lebensmittel'),
+            _buildListTile(title: 'Vorratskammer'),
+            _buildListTile(title: 'Meine Rezepte'),
+            _buildListTile(title: 'Rezeptvorschläge'),
+            _buildListTile(title: 'Ist das noch gut?'),
+            _buildListTile(title: 'Savior Score'),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildListTile({required String title}) {
+    return ListTile(
+      dense: true,
+      title: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Color.fromRGBO(8, 83, 59, 1.0),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      //onTap: () {
+      //  Navigator.pop(context);
+      //},
     );
   }
 }
