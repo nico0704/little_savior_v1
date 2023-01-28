fce94e8468bfdf38dd3e53aa46299fb320bcf125
nico0704 <nico.schroeder@informatik.hs-fulda.de>
2023-01-29 00:17:55 +0100

    created stock api to fetch stocks from database

---
+++ lib/models/stock.api.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:little_savior_v1/models/stock.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+class StockApi {
+  static Future<List<Stock>> getStocks() async {
+    var uri = Uri.http("medsrv.informatik.hs-fulda.de", "/lsbackend/api/v1/stocks", {'format': 'json'});
+    final response = await http.get(uri);
+    List<dynamic> dataList = jsonDecode(response.body);
+    print(response.body);
+    return Stock.recipesFromSnapshot(dataList);
+  }
+}
