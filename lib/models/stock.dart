cc29ca5e5b0540284e82b40c7e481a1ef9d4c2c2
nico0704 <nico.schroeder@informatik.hs-fulda.de>
2023-01-29 00:16:58 +0100

    created model to represent stock data

---
+++ lib/models/stock.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+List<Stock> stockFromJson(String str) =>
+    List<Stock>.from(json.decode(str).map((x) => Stock.fromJson(x)));
+
+String stockToJson(List<Stock> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Stock {
+  Stock({
+    required this.id,
+    required this.name,
+    required this.stores,
+  });
+
+  int id;
+  String name;
+  List<int> stores;
+
+  factory Stock.fromJson(Map<String, dynamic> json) => Stock(
+        id: json["id"],
+        name: json["name"],
+        stores: List<int>.from(json["stores"].map((x) => x)),
+      );
+
+  static List<Stock> recipesFromSnapshot(List snapshot) {
+    return snapshot.map((data) {
+      return Stock.fromJson(data);
+    }).toList();
+  }
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "stores": List<dynamic>.from(stores.map((x) => x)),
+      };
+}
