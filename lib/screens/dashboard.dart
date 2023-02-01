a90bbde6e3a2456bc697a6224a596f560d4c3149
MoritzOtterbein <moritz.otterbein@informatik.hs-fulda.de>
2023-02-01 22:56:34 +0100

    crossAxisAlignment: CrossAxisAlignment.end, SUCKT

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -16,6 +16,18 @@ class Dashboard extends StatelessWidget {
         body: Center(
           child: Column(
             children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height:200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      color: Palette.terraCotta,
+                      borderRadius: BorderRadius.circular(100),
+                    //more than 50% of width makes circle
+                  ),
+                ),
+              ),
               Text('Läuft diese Woche ab',
                   style: TextStyle(color: Color.fromRGBO(8, 83, 59, 1.0))),
               SizedBox(
@@ -29,23 +41,26 @@ class Dashboard extends StatelessWidget {
                   fontSize: 18,
                   color: Palette.castletonGreen,
               ),),
-              SizedBox(
-                width: 300,
-                height: 40,
-                child: ElevatedButton(style: ElevatedButton.styleFrom(
-                    backgroundColor: Palette.castletonGreen,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
+              Padding(
+                padding: const EdgeInsets.only(top: 250),
+                child: SizedBox(
+                  width: 300,
+                  height: 40,
+                  child: ElevatedButton(style: ElevatedButton.styleFrom(
+                      backgroundColor: Palette.castletonGreen,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                    ),
+
                   ),
+                    onPressed:(){
+                    Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyRecipes()), //Hier Muss die Seite des Rezeptes noch rein
+                  );
+                  },
+                    child: Text("Rezept ansehen"),
 
-                ),
-                  onPressed:(){
-                  Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => MyRecipes()), //Hier Muss die Seite des Rezeptes noch rein
-                );
-                },
-                  child: Text("Rezept ansehen"),
-
+                  ),
                 ),
               )
             ],
