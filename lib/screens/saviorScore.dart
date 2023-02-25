c7eadb7f8de8d8b6410863cc67aa478437597eeb
nico0704 <nico.schroeder@informatik.hs-fulda.de>
2023-02-26 00:23:37 +0100

    addded numbers in circles

--- lib/screens/saviorScore.dart
+++ lib/screens/saviorScore.dart
@@ -16,39 +16,77 @@ class _SaviorScoreState extends State<SaviorScore> {
     return Scaffold(
         appBar: const Menue().getAppBar(title: "Savior Score"),
         drawer: const Menue().getDrawer(context),
-        body: Container(
-          //color: Palette.macaroniAndCheeseHalf,
-          child: Padding(
-            padding: const EdgeInsets.all(50),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
+        body: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 85,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      color: Palette.terraCotta,
+                      shape: BoxShape.circle,
+                    ),
+                    child:Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('5', style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        )),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "weggeworfen",
+                      style: TextStyle(
                         color: Palette.terraCotta,
-                        shape: BoxShape.circle,
                       ),
                     ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
+                  )
+                ],
+              ),
+              SizedBox(
+                width: 50,
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Palette.illuminatingEmerald,
+                      shape: BoxShape.circle,
+                    ),
+                    child:Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('13', style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        )),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                        "gerettet",
+                      style: TextStyle(
                         color: Palette.illuminatingEmerald,
-                        shape: BoxShape.circle,
                       ),
                     ),
-                  ],
-                )
-              ],
-            ),
+                  )
+                ],
+              ),
+            ],
           ),
         ));
   }
