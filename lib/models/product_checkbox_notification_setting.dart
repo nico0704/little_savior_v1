8240587308adc9695337539ebb04b49a00d76c58
nico0704 <nico.schroeder@informatik.hs-fulda.de>
2023-02-25 13:36:23 +0100

    added expiry attribute

--- lib/models/product_checkbox_notification_setting.dart
+++ lib/models/product_checkbox_notification_setting.dart
@@ -1,11 +1,13 @@
 class ProductCheckboxNotificationSetting {
   String title;
   bool value;
+  int expiry;
   DateTime bbd;
 
   ProductCheckboxNotificationSetting({
     required this.title,
     this.value = false,
+    required this.expiry,
     required this.bbd,
   });
 }
--- lib/screens/myStockIngredients.dart
+++ lib/screens/myStockIngredients.dart
@@ -19,33 +19,33 @@ class _MyStockIngredientsState extends State<MyStockIngredients> {
   late List<Product> _products;
   List<ProductCheckboxNotificationSetting> productNotificationsRed = [
     ProductCheckboxNotificationSetting(
-        title: "Produkt1", bbd: DateTime.now().add(new Duration(days: 3))),
+        title: "Produkt1", expiry: 3, bbd: DateTime.now().add(new Duration(days: 3))),
     ProductCheckboxNotificationSetting(
-        title: "Produkt2", bbd: DateTime.now().add(new Duration(days: 3))),
+        title: "Produkt2", expiry: 3, bbd: DateTime.now().add(new Duration(days: 3))),
     ProductCheckboxNotificationSetting(
-        title: "Produkt3", bbd: DateTime.now().add(new Duration(days: 3))),
+        title: "Produkt3", expiry: 3, bbd: DateTime.now().add(new Duration(days: 3))),
     ProductCheckboxNotificationSetting(
-        title: "Produkt4", bbd: DateTime.now().add(new Duration(days: 3)))
+        title: "Produkt4", expiry: 3, bbd: DateTime.now().add(new Duration(days: 3)))
   ];
   List<ProductCheckboxNotificationSetting> productNotificationsYellow = [
     ProductCheckboxNotificationSetting(
-        title: "Produkt5", bbd: DateTime.now().add(new Duration(days: 10))),
+        title: "Produkt5", expiry: 10, bbd: DateTime.now().add(new Duration(days: 10))),
     ProductCheckboxNotificationSetting(
-        title: "Produkt6", bbd: DateTime.now().add(new Duration(days: 10))),
+        title: "Produkt6", expiry: 10, bbd: DateTime.now().add(new Duration(days: 10))),
     ProductCheckboxNotificationSetting(
-        title: "Produkt7", bbd: DateTime.now().add(new Duration(days: 10))),
+        title: "Produkt7", expiry: 10, bbd: DateTime.now().add(new Duration(days: 10))),
     ProductCheckboxNotificationSetting(
-        title: "Produkt8", bbd: DateTime.now().add(new Duration(days: 10)))
+        title: "Produkt8", expiry: 10, bbd: DateTime.now().add(new Duration(days: 10)))
   ];
   List<ProductCheckboxNotificationSetting> productNotificationsGreen = [
     ProductCheckboxNotificationSetting(
-        title: "Produkt9", bbd: DateTime.now().add(new Duration(days: 20))),
+        title: "Produkt9", expiry: 20, bbd: DateTime.now().add(new Duration(days: 20))),
     ProductCheckboxNotificationSetting(
-        title: "Produkt10", bbd: DateTime.now().add(new Duration(days: 20))),
+        title: "Produkt10", expiry: 20, bbd: DateTime.now().add(new Duration(days: 20))),
     ProductCheckboxNotificationSetting(
-        title: "Produkt11", bbd: DateTime.now().add(new Duration(days: 20))),
+        title: "Produkt11", expiry: 20, bbd: DateTime.now().add(new Duration(days: 20))),
     ProductCheckboxNotificationSetting(
-        title: "Produkt12", bbd: DateTime.now().add(new Duration(days: 20)))
+        title: "Produkt12", expiry: 20, bbd: DateTime.now().add(new Duration(days: 20)))
   ];
   // end of test data
   @override
